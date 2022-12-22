7d63937953af8a65482cd6a69f85902626fb1f5f
Slava <viachaslau.pailak@gmail.com>
2022-12-23 00:02:44 +0300

    Create context menu components

---
+++ react-spotify/src/components/PlaylistContextMenuItem.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const PlaylistContextMenuItem = ({children: label}) => {
+  return (
+    <li>
+      <button className="w-full p-3 text-left hover:text-white hover:bg-[#3e3e3e] cursor-default">
+        {label}
+      </button>
+    </li>
+  );
+};
+
+export default PlaylistContextMenuItem;
---
+++ react-spotify/src/components/PlaylistContextSubmenu.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import PlaylistContextMenuItem from "./PlaylistContextMenuItem";
+
+const subMenuItems = [
+    {
+        label: 'Copy link to playlist',
+    },
+    {
+        label: 'Embed playlist',
+    }
+]
+
+const PlaylistContextSubmenu = () => {
+  return (
+    <ul className="absolute top-0 left-full bg-[#282828] text-[#eaeaea] text-sm p-1 rounded shadow-xl cursor-default">
+      {subMenuItems.map(({label}) => (
+        <PlaylistContextMenuItem key={label}>{label}</PlaylistContextMenuItem>
+      ))}
+    </ul>
+  );
+};
+
+export default PlaylistContextSubmenu;
