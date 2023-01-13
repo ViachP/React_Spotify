29e447abc1c20d3030d87b092fdfddfe245e3b03
Slava <viachaslau.pailak@gmail.com>
2023-01-13 19:12:45 +0300

    Consolidate menu item state

--- react-spotify/src/components/PlaylistContextMenuItem.jsx
+++ react-spotify/src/components/PlaylistContextMenuItem.jsx
@@ -3,53 +3,66 @@ import PlaylistContextMenu from "./PlaylistContextMenu";
 import { ChevronRightIcon } from "@heroicons/react/24/outline";
 
 const PlaylistContextMenuItem = ({ children: label, subMenuItems }) => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [menuPossitionXClass, setMenuPossitionXClass] = useState('left-full');
-  const [menuPossitionYClass, setMenuPossitionYClass] = useState('top-0');
+  const [menuState, setMenuState] = useState({
+    isOpen: false,
+    positionClasses: "",
+  });
   const menuItemRef = useRef(null);
 
-  function getMenuPossitionXClass () {
-
+  function getMenuPositionXClass() {
     const menuItem = menuItemRef.current;
     const menuItemWidth = menuItem.offsetWidth;
     const windowWidth = window.innerWidth;
     const menuItemRightCoordX = menuItem.getBoundingClientRect().right;
-    const shouldMoveMenuLeft = menuItemWidth > windowWidth - menuItemRightCoordX;
+    const shouldMoveMenuLeft =
+      menuItemWidth > windowWidth - menuItemRightCoordX;
 
-    return  shouldMoveMenuLeft ? 'right-full' : 'left-full';
+    return shouldMoveMenuLeft ? "right-full" : "left-full";
   }
 
-  function getMenuPossitionYClass () {
-
+  function getMenuPositionYClass() {
     const windowHeight = window.innerHeight;
     const menuItem = menuItemRef.current;
     const menuHeight = menuItem.offsetHeight * subMenuItems.length;
     const menuItemBottomCoordY = menuItem.getBoundingClientRect().bottom;
     const shouldMoveMenuUp = menuHeight > windowHeight - menuItemBottomCoordY;
 
-    return  shouldMoveMenuUp ? 'bottom-0' : 'top-0';
+    return shouldMoveMenuUp ? "bottom-0" : "top-0";
   }
 
-  function openMenu () {
-    setIsMenuOpen(true);
-    setMenuPossitionXClass(getMenuPossitionXClass());
-    setMenuPossitionYClass(getMenuPossitionYClass());
+  function getMenuPositionClasses() {
+    return `${getMenuPositionYClass()} ${getMenuPositionXClass()}`;
   }
-  function closeMenu () {
-    setIsMenuOpen(false);
 
+  function openMenu() {
+    setMenuState({
+      isOpen: true,
+      positionClasses: getMenuPositionClasses(),
+    });
+  }
+
+  function closeMenu() {
+    setMenuState({
+      isOpen: false,
+      positionClasses: "",
+    });
   }
 
   if (subMenuItems) {
     return (
-      <li className="relative" onMouseEnter={openMenu} onMouseLeave={closeMenu} ref={menuItemRef}>
+      <li
+        className="relative"
+        onMouseEnter={openMenu}
+        onMouseLeave={closeMenu}
+        ref={menuItemRef}
+      >
         <button className="w-full p-3 text-left hover:text-white hover:bg-[#3e3e3e] cursor-default flex justify-between items-center">
           {label} <ChevronRightIcon className="h-4 w-4" />
         </button>
-        {isMenuOpen && (
+        {menuState.isOpen && (
           <PlaylistContextMenu
             menuItems={subMenuItems}
-            classes={`bg-[#282828] text-[#eaeaea] text-sm p-1 rounded shadow-xl cursor-default absolute ${menuPossitionYClass} ${menuPossitionXClass}`}
+            classes={`bg-[#282828] text-[#eaeaea] text-sm p-1 rounded shadow-xl cursor-default absolute ${menuState.positionClasses}`}
           />
         )}
       </li>
