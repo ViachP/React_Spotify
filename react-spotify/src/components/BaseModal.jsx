0200f42b6b1ce642679b161336a1aef5254ef13a
Slava <viachaslau.pailak@gmail.com>
2023-02-20 23:47:09 +0300

    Animate BaseModal closing

--- react-spotify/src/components/BaseModal.jsx
+++ react-spotify/src/components/BaseModal.jsx
@@ -12,7 +12,7 @@ function BaseModal({ onClose: handleClose }) {
     });
 
     function handleEsc({ key }) {
-      if (key === "Escape") handleClose();
+      if (key === "Escape") close();
     }
 
     document.addEventListener("keydown", handleEsc);
@@ -20,12 +20,19 @@ function BaseModal({ onClose: handleClose }) {
     return () => document.removeEventListener("keydown", handleEsc);
   });
 
+   function close() {
+    ref.current.classList.add('opacity-0');
+    contentRef.current.classList.add('-translate-y-10');
+
+    setTimeout(handleClose, 500);
+  }
+
   return (
     <div
       className="fixed inset-0 bg-black/70 z-30 flex justify-center items-center opacity-0 transition-opacity duration-500"
       role="dialog"
       ref={ref}
-      onClick={handleClose}
+      onClick={close}
     >
       <div
         className="relative bg-[#333] h-1/3 w-2/5 rounded-xl -translate-y-10 transition-transform duration-500"
@@ -34,7 +41,7 @@ function BaseModal({ onClose: handleClose }) {
       >
         <button
           className="absolute right-0 p-3 text-neutral-500 hover:text-neutral-200"
-          onClick={handleClose}
+          onClick={close}
         >
           <XMarkIcon className="h-8 w-8" />
         </button>
